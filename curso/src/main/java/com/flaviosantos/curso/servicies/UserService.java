eb6e7fb79f0245ff0f46008eaf72aee8efa1d36e
FlavioSantos2002 <luizflavio.20052002@gmail.com>
2023-07-27 23:15:55 -0400

    new commit

---
+++ curso/src/main/java/com/flaviosantos/curso/servicies/UserService.java
@@ -0,0 +1,27 @@
+package com.flaviosantos.curso.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flaviosantos.curso.entities.Usuario;
+import com.flaviosantos.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<Usuario> findAll(){
+		return repository.findAll();
+	}
+	
+	public Usuario findById(Long id) {
+		Optional<Usuario> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
